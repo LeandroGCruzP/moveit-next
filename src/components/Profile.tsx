40ff7feb642944727d9f7d2db819ce8fd3aea261
Leandro Geovanni Cruz <leandro.programmer@hotmail.com>
2021-02-24 07:13:26 -0300

    create project with Next.js and create firts components

---
+++ src/components/Profile.tsx
@@ -0,0 +1,16 @@
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+  return (
+    <div className={styles.profileContainer}>
+      <img
+        src="https://avatars.githubusercontent.com/u/59587859?s=460&u=e2c61934c682f1bc9a5d07dfb9cb172bf3cf8b9c&v=4"
+        alt="Leandro Cruz"
+      />
+      <div>
+        <strong>Leandro Cruz</strong>
+        <p>Level 1</p>
+      </div>
+    </div>
+  );
+}
