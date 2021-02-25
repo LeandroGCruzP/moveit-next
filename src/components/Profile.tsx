91de79432ab0f80f5cd588221772d06885e9ea2e
Leandro Geovanni Cruz <leandro.programmer@hotmail.com>
2021-02-25 07:14:14 -0300

    add Countdown ad style home

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -9,7 +9,10 @@ export function Profile() {
       />
       <div>
         <strong>Leandro Cruz</strong>
-        <p>Level 1</p>
+        <p>
+          <img src="icons/level.svg" alt="Level" />
+          Level 1
+        </p>
       </div>
     </div>
   );
